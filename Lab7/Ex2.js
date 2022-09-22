76f2feba8e341fa9e690a81c679c75897f45490b
D. Port Me <dport@hawaii.edu>
2022-09-22 11:49:59 -1000

    xxx

---
+++ Lab7/Ex2.js
@@ -0,0 +1,11 @@
+require("./products_data.js");
+var num_products = 5;
+var prod_num = 0;
+while(prod_num++ < num_products) {
+    
+    if( (prod_num > num_products*.75) || (prod_num < num_products*.25) ) {
+        console.log(`${eval('name' + prod_num )} is sold out!`);
+        continue;
+    }
+    console.log( prod_num);
+}
