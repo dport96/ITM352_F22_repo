6bda78e28a9c0b0f4f7e6f844b653ebb96d49f90
D. Port Me <dport@hawaii.edu>
2022-10-11 10:39:20 -1000

    Create Ex1.js

---
+++ Lab10/Ex1.js
@@ -0,0 +1,14 @@
+var year = 1966;
+var month = 5;
+var day = 14;
+
+step1 = 66;
+step2 = parseInt(66/4);
+step3 = step1 + step2;
+step4 = 1;
+step6 = step4 + step3;
+step7 = day + step6;
+step8 = step7;
+step9 = step8%7;
+dob = 'saturday';
+console.log(step9);
