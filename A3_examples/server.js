57f9009aba8962c98b336d7c4da157234889c9ac
D. Port Me <dport@hawaii.edu>
2022-12-08 11:54:21 -1000

    ""

--- A3_examples/server.js
+++ A3_examples/server.js
@@ -19,6 +19,13 @@ app.post("/get_products_data", function (request, response) {
     response.json(products_data);
 });
 
+app.post("/addToCart", function (request, response) {
+    var selections = JSON.parse(request.query);
+    response.send(selections);
+   //  response.json(products_data);
+});
+
+
 app.get("/add_to_cart", function (request, response) {
     var products_key = request.query['products_key']; // get the product key sent from the form post
     var quantities = request.query['quantities'].map(Number); // Get quantities from the form post and convert strings from form post to numbers
